02c0510f77646b6709fbe649fc30f8bfbf98cde4
felipebcarlos <48163995+felipebcarlos@users.noreply.github.com>
2025-11-27 22:46:31 -0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -57,7 +57,7 @@ public class DemoApplication {
 		return "https://www.autoglassonline.com.br/";
 	}
 	
-	@GetMapping("/10CLDR")
+	@GetMapping("/10CLDR_Devops")
 	String cldr(){
 		return "Turma 10 MBA Cloud";
 	}
